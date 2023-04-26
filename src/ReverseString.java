2b07b178314ff98ea9cd6bad437dc468a30d4b7c
preet <pritishreefeb@gmail.com>
2023-04-26 20:46:12 +0530

    just tried

---
+++ src/ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String s ="kanungo";
+        String rev= "";
+        int len =s.length();
+        for(int i =len-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+        System.out.println(rev);
+
+    }
+}
